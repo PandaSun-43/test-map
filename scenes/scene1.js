9f552a3c355c9bc0fdb998577c76fac47f801984
PandaSun-43 <yiwen.hu369@gmail.com>
2026-02-03 19:54:57 +0800

    Add files via upload

---
+++ scenes/scene1.js
@@ -0,0 +1,17 @@
+var scene1 = {
+    "type": "equirectangular",
+    "panorama": "./images/1.jpg",
+    "hotSpots": [
+        {
+            "pitch": 0, "yaw": 0,
+            "type": "scene",
+            "text": "Go to Spot 2",
+            "sceneId": "scene2"
+        },
+        {
+            "pitch": 5, "yaw": 0, 
+            "type": "info",
+            "text": "Lion Grove is one of the most famous classical Jiangnan Gardens known for its rockery maze and Zen culture. It has been recognized in 1997 as a UNESCO World Heritage, along with other Suzhou gardens like Humble Administrator's Garden, Lingering Garden, and Canglang Pavilion."
+        }
+    ]
+};
